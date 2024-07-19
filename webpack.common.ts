7fd625806c9ba0717f8f3d1d35c1ef67d00362d8
Siva Ganesh <akulasivaganesh1996@gmail.com>
2024-07-19 18:01:03 +0530

    add some build configuration to test the file buildig

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,7 +1,12 @@
 import { Configuration } from "webpack";
+import path from "path";
 
 const config: Configuration = {
   entry: "./src/index.ts",
-  output: {},
+  mode: "development",
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
 };
 export default config;
