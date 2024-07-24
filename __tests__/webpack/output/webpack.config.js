9ad1d54559c4fc30aba758a6b92ebf32fd0ffa42
Siva Ganesh <akulasivaganesh1996@gmail.com>
2024-07-24 19:40:41 +0530

    added simple output config tests

---
+++ __tests__/webpack/output/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  /**
+   * Single Entry (Shorthand) Syntax
+   */
+  // single entry file with string input file
+  // entry: "../src/basicCalc.js",
+
+  /**
+   * Object Syntax
+   */
+  // basic entry without description object
+  entry: {
+    basicCalc: "../src/basicCalc.js",
+    trigonometryTable: "../src/trigonometryTable.js",
+  },
+
+  mode: "development",
+  devtool: false,
+  output: {
+    // single entry file with string input file
+    // filename: "singleEntryString.js",
+
+    // basic entry without description object
+    filename: "[name].objectSyntaxBasicEntry.js",
+
+    // TODO: publicPath
+
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
