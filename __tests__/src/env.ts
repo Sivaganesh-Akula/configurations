31c14d3b45fd5e9119ba4e833d637399d9826b2a
Siva Ganesh <akulasivaganesh1996@gmail.com>
2024-07-26 22:56:21 +0530

    removed unwanted files in tests

--- __tests__/src/env.ts
+++
@@ -1,4 +0,0 @@
-(function () {
-  const localVar = () => "This is a local variable";
-  console.log(localVar()); // Output: This is a local variable
-})();
--- __tests__/src/overrides.ts
+++
@@ -1,5 +0,0 @@
-(function () {
-  const localVar = (someLongVarName) =>
-    someLongVarName + "I am a local variable";
-  console.log(localVar("Hello")); // Output: This is a local variable
-})();
